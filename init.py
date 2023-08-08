24bb9753d7313cc5870d46ea3431382f53aab21a
bsflll <106779538+bsflll@users.noreply.github.com>
2023-08-08 10:56:12 -0400

    Create init.py

---
+++ init.py
@@ -0,0 +1,16 @@
+from IPython.utils.path import random
+L = 200 # length of sequence
+
+N_train = 5000 #5000
+N_test = 5000
+N_total = N_train + N_test # number of total test cases
+
+import numpy as np
+
+#Omega1 = 1/10
+#Omega2 = 1/20
+
+dim_in = 1
+dim_out = 1
+
+m = 10 # delay
